251dd2660e9558e5f352c0130a6091e1d77be05b
Lenora Porter <lporter630@gmail.com>
2024-04-13 21:44:13 -0400

    Add app structure and data-table component from TanStack

---
+++ app/resources/columns.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Resource = {
+  id: string
+  name: string
+  ipAddress: number
+  status: "active" | "inactive"
+  dropletType: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<Resource>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "ipAddress",
+    header: "IP Address",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created",
+  },
+]
